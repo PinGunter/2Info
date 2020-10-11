81bf25aab67353d074c4de6cffd7caf026dd00c7
salvadotcom <salvaromero08@gmail.com>
2020-10-11 12:00:37 +0200

    Ejercicio Propuesto Pila con vector estatico

---
+++ ED/EjerciciosPropuestos/PilaVectorEstatico/Pila.h
@@ -0,0 +1,27 @@
+#ifndef PILA_H
+#define PILA_H
+typedef char Tbase;
+const int TAM = 500;
+class Pila
+{
+private:
+	Tbase datos[TAM];
+	int nelem;
+
+	void copiar(const Pila& p);
+public:
+	Pila();
+	Pila(const Pila& p);
+	~Pila() = default;
+	Pila & operator=(const Pila& p);
+
+	bool vacia() const;
+	void poner(const Tbase& c);
+	void quitar();
+	Tbase& tope();
+	const Tbase& tope() const;
+	Pila & operator--(int n);
+	void operator+=(const Tbase& c);
+};
+
+#endif
