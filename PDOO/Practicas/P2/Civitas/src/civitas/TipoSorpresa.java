833566383b442feacdcdaa9423b2eb6f325000c3
PinGunter <salvaromero08@gmail.com>
2020-10-13 12:14:08 +0200

    Cambio de carpetas y añadidad p2

--- PDOO/Practicas/P2/Civitas/src/civitas/TipoSorpresa.java
+++ PDOO/Practicas/P2/Civitas/src/civitas/TipoSorpresa.java
@@ -9,13 +9,6 @@ package civitas;
  *
  * @author salva
  */
-public class Civitas {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+enum TipoSorpresa {
+    IRCARCEL, IRCASILLA, PAGARCOBRAR, PORCASAHOTEL, PORJUGADOR, SALIRCARCEL
 }
