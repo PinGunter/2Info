c70505d884d4f4504de7f5f5e38b09609b1b0ad6
PinGunter <salvaromero08@gmail.com>
2020-10-26 15:38:17 +0100

    solucion lifo hecha

--- SCD/Seminario2/scd-s2-fuentes/prodcons_lifo_monitor.cpp
+++ SCD/Seminario2/scd-s2-fuentes/prodcons_lifo_monitor.cpp
@@ -11,20 +11,21 @@
 #include <mutex>
 #include <condition_variable>
 #include <random>
-#include "HoareMonitor.h"   //clase para monitores SU
+#include "HoareMonitor.h" //clase para monitores SU
 
 using namespace std;
-using namespace HM;         //espacio de nombres para los monitores
+using namespace HM; //espacio de nombres para los monitores
 
 mutex mtx; //cerrojo para mostrar en pantalla
 
 // ========================================================================
 /* FUNCIONES PRODUCTORAS/CONSUMIDORAS Y DE COMPROBACION*/
 
+const int num_hebras = 4;
 const int num_items = 40;
 unsigned
-   cont_prod[num_items], // contadores de verificación: producidos
-   cont_cons[num_items]; // contadores de verificación: consumidos
+    cont_prod[num_items * num_hebras], // contadores de verificación: producidos
+    cont_cons[num_items * num_hebras]; // contadores de verificación: consumidos
 
 //**********************************************************************
 // plantilla de función para generar un entero aleatorio uniformemente
@@ -32,49 +33,50 @@ unsigned
 // (ambos tienen que ser dos constantes, conocidas en tiempo de compilación)
 //----------------------------------------------------------------------
 
-template< int min, int max > int aleatorio()
+template <int min, int max>
+int aleatorio()
 {
-  static default_random_engine generador( (random_device())() );
-  static uniform_int_distribution<int> distribucion_uniforme( min, max ) ;
-  return distribucion_uniforme( generador );
+   static default_random_engine generador((random_device())());
+   static uniform_int_distribution<int> distribucion_uniforme(min, max);
+   return distribucion_uniforme(generador);
 }
 
 //**********************************************************************
 // funciones comunes a las dos soluciones (fifo y lifo)
 //----------------------------------------------------------------------
 
-int producir_dato()
+int producir_dato(int indice)
 {
-   static int contador = 0 ;
-   this_thread::sleep_for( chrono::milliseconds( aleatorio<20,100>() ));
-   mtx.lock();
-   cout << "producido: " << contador << endl << flush ;
-   mtx.unlock();
-   cont_prod[contador] ++ ;
-   return contador++ ;
+   static int contador = 0;
+   this_thread::sleep_for(chrono::milliseconds(aleatorio<20, 100>()));
+   unique_lock<mutex> guarda(mtx);
+   cout << "productor " << indice << " produce: " << contador << endl
+        << flush;
+   cont_prod[contador]++;
+   return contador++;
 }
 //----------------------------------------------------------------------
 
-void consumir_dato( unsigned dato )
+void consumir_dato(unsigned dato, int indice)
 {
-   if ( num_items <= dato )
+   if (num_items * num_hebras <= dato)
    {
-      cout << " dato === " << dato << ", num_items == " << num_items << endl ;
-      assert( dato < num_items );
+      cout << " dato === " << dato << ", num_items == " << num_items << endl;
+      //assert(dato < num_items*num_hebras);
    }
-   cont_cons[dato] ++ ;
-   this_thread::sleep_for( chrono::milliseconds( aleatorio<20,100>() ));
-   mtx.lock();
-   cout << "                  consumido: " << dato << endl ;
-   mtx.unlock();
+   cont_cons[dato]++;
+   this_thread::sleep_for(chrono::milliseconds(aleatorio<20, 100>()));
+   unique_lock<mutex> guarda(mtx);
+   cout << "                  consumidor " << indice << " consume: " << dato << endl;
 }
 //----------------------------------------------------------------------
 
 void ini_contadores()
 {
-   for( unsigned i = 0 ; i < num_items ; i++ )
-   {  cont_prod[i] = 0 ;
-      cont_cons[i] = 0 ;
+   for (unsigned i = 0; i < num_items * num_hebras; i++)
+   {
+      cont_prod[i] = 0;
+      cont_cons[i] = 0;
    }
 }
 
@@ -82,24 +84,26 @@ void ini_contadores()
 
 void test_contadores()
 {
-   bool ok = true ;
-   cout << "comprobando contadores ...." << flush ;
+   bool ok = true;
+   cout << "comprobando contadores ...." << flush;
 
-   for( unsigned i = 0 ; i < num_items ; i++ )
+   for (unsigned i = 0; i < num_items; i++)
    {
-      if ( cont_prod[i] != 1 )
+      if (cont_prod[i] != 1)
       {
-         cout << "error: valor " << i << " producido " << cont_prod[i] << " veces." << endl ;
-         ok = false ;
+         cout << "error: valor " << i << " producido " << cont_prod[i] << " veces." << endl;
+         ok = false;
       }
-      if ( cont_cons[i] != 1 )
+      if (cont_cons[i] != 1)
       {
-         cout << "error: valor " << i << " consumido " << cont_cons[i] << " veces" << endl ;
-         ok = false ;
+         cout << "error: valor " << i << " consumido " << cont_cons[i] << " veces" << endl;
+         ok = false;
       }
    }
    if (ok)
-      cout << endl << flush << "solución (aparentemente) correcta." << endl << flush ;
+      cout << endl
+           << flush << "solución (aparentemente) correcta." << endl
+           << flush;
 }
 
 //=====================================================================000
@@ -108,88 +112,88 @@ void test_contadores()
  * @brief clase que representa el monitor de productores-consumidores
  */
 
-class ProdConsLIFO : public HoareMonitor{
+class ProdConsLIFO : public HoareMonitor
+{
 private:
-    static const int num_celdas_total = 10;
-    int buffer[num_celdas_total];
-    int primera_libre;
-    // int n_ocupadas, n_libres;
-    CondVar ocupadas, libres;
+   static const int num_celdas_total = 10;
+   int buffer[num_celdas_total];
+   int primera_libre;
+   // int n_ocupadas, n_libres;
+   CondVar ocupadas, libres;
 
 public:
-    ProdConsLIFO();
-    void insertar(int dato);
-    int extraer();
+   ProdConsLIFO();
+   void insertar(int dato);
+   int extraer();
 };
 
-ProdConsLIFO::ProdConsLIFO(){
-    primera_libre = 0;
-    ocupadas = newCondVar();
-    libres = newCondVar();
-    // n_ocupadas = 0;
-    // n_libres = num_celdas_total;
+ProdConsLIFO::ProdConsLIFO()
+{
+   primera_libre = 0;
+   ocupadas = newCondVar();
+   libres = newCondVar();
+   for (int i = 0; i < num_celdas_total; i++)
+      buffer[i] = 0;
 }
 
-//metodo para insertar valores en el buffer 
-void ProdConsLIFO::insertar(int dato){
-    if (primera_libre >= num_celdas_total)
-        libres.wait();
-    else{
-        buffer[primera_libre] = dato;
-        primera_libre ++;
-        ocupadas.signal();
-        mtx.lock();
-        cout << "Se inserta " << dato << " en buffer" << endl;
-        mtx.unlock();
-    }
+//metodo para insertar valores en el buffer
+void ProdConsLIFO::insertar(int dato)
+{
+   while (primera_libre >= num_celdas_total)
+      libres.wait();
+
+   buffer[primera_libre] = dato;
+   primera_libre++;
+   ocupadas.signal();
 }
 
 //metodo para leer valores del buffer
-int ProdConsLIFO::extraer(){
-    if (primera_libre == 0)
-        ocupadas.wait();
-    else{
-        primera_libre--;
-        int valor = buffer[primera_libre];
-        libres.signal();
-        return valor;
-    }
+int ProdConsLIFO::extraer()
+{
+   while (primera_libre == 0)
+      ocupadas.wait();
+   primera_libre--;
+   int valor = buffer[primera_libre];
+   libres.signal();
+   return valor;
 }
 
 //funciones que ejecutan las hebras
-void funcion_hebra_productora( ProdConsLIFO * monitor,int indice )
+void funcion_hebra_productora(MRef<ProdConsLIFO> monitor, int indice)
 {
-   for( unsigned i = 0 ; i < num_items ; i++ )
+   for (unsigned i = 0; i < num_items; i++)
    {
-      int valor = producir_dato() ;
-      monitor->insertar( valor );
+      int valor = producir_dato(indice);
+      monitor->insertar(valor);
    }
 }
 
-void funcion_hebra_consumidora( ProdConsLIFO * monitor ,int indice)
+void funcion_hebra_consumidora(MRef<ProdConsLIFO> monitor, int indice)
 {
-   for( unsigned i = 0 ; i < num_items ; i++ )
+   for (unsigned i = 0; i < num_items; i++)
    {
       int valor = monitor->extraer();
-      consumir_dato( valor ) ;
+      consumir_dato(valor, indice);
    }
 }
 
 int main()
 {
-   const int num_hebras = 100,
-             num_hebras_cita = 5 ;
-
    // crear monitor
-   ProdConsLIFO monitor();
-
+   MRef<ProdConsLIFO> monitor = Create<ProdConsLIFO>();
    // crear y lanzar hebras
-   thread hebra_prod[num_hebras], hebra_prod[num_hebras];
-   for( unsigned i = 0 ; i < num_hebras ; i++ ){
-      hebra[i] = thread( funcion_hebra_productora, &monitor, i );
-      hebra[i]
-
+   thread hebra_prod[num_hebras], hebra_cons[num_hebras];
+   for (int i = 0; i < num_hebras; i++)
+   {
+      hebra_prod[i] = thread(funcion_hebra_productora, monitor, i);
+      hebra_cons[i] = thread(funcion_hebra_consumidora, monitor, i);
+   }
    // esperar a que terminen las hebras (no pasa nunca)
-   for( unsigned i = 0 ; i < num_hebras ; i++ )
-      hebra[i].join();
+   for (unsigned i = 0; i < num_hebras; i++)
+   {
+      hebra_prod[i].join();
+      hebra_cons[i].join();
+   }
+
+   test_contadores();
 }
