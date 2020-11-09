7afdc1e4da535d279f7e09ef34264f55896511e8
PinGunter <salvaromero08@gmail.com>
2020-11-10 00:14:56 +0100

    ejercicio 2 s1 mod2 so

--- SO/Practicas/Mod2/SO-P-Todos_MaterialModulo2/Sesion1/ejercicio2.c
+++ SO/Practicas/Mod2/SO-P-Todos_MaterialModulo2/Sesion1/ejercicio2.c
@@ -10,8 +10,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <errno.h>
-#include <string.h>
-
+#include <math.h>
 const int num_bytes = 80;
 
 int main(int argc, char *argv[])
@@ -20,9 +19,10 @@ int main(int argc, char *argv[])
 	int fdout; // salida.txt
 	char buffer[num_bytes];
 	int leidos;
-	char cont = '1';
+	int cont = 1;
 	struct stat st;
 	int size;
+
 
 	if (argc == 2)
 	{
@@ -48,33 +48,23 @@ int main(int argc, char *argv[])
 		exit(-1);
 	}
 
-	size = size / num_bytes;
 	printf("%i",size);
-	char tamanio = size + '0';
+	size = ceil(size*1.0/num_bytes);
 	leidos = read(fdin,buffer,num_bytes+1);
 	if (leidos > 0){
-		char total [] = "Numero total de bloques: ";
-		write(fdout, total, sizeof(total)-1);
-		write(fdout,&tamanio,1);
-		write(fdout,"\n",1);
+		char total [50];
+		int tamanio = sprintf(total, "Numero total de bloques %i\n", size);
+		write(fdout,total,tamanio);
 	}
 	while (leidos > 0)	{
-		char bloque [] = "Bloque ";
-		write(fdout,bloque,sizeof(bloque)-1);
-		write(fdout,&cont,1);
-		write(fdout,"\n",1);
-
+		char bloque [50];
+		int tamanio_bloque = sprintf(bloque, "Bloque: %i\n", cont);
+		write(fdout, bloque, tamanio_bloque);
 		int escritos =	write(fdout,buffer, leidos);
 		cont++;
 		leidos = read(fdin,buffer,num_bytes+1);
 	}
 	//ponemos al principio el puntero de escritura
-	cont--;
-	lseek(fdout,-cont*num_bytes,SEEK_CUR);
-	char bloque [] = "El numero de bloque es ";
-	write(fdout,bloque,sizeof(bloque)-1);
-	write(fdout,&cont,1);
-	write(fdout,"\n",1);	
 	close(fdin);
 	close(fdout);
 	return 0;
