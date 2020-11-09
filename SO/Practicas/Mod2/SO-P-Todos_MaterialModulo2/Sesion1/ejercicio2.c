9f1020117518cb59b1905e2fea2cd3e256e8c29e
PinGunter <salvaromero08@gmail.com>
2020-11-09 23:48:34 +0100

    ejercicio 2 s1 mod 2 so

--- SO/Practicas/Mod2/SO-P-Todos_MaterialModulo2/Sesion1/ejercicio2.c
+++ SO/Practicas/Mod2/SO-P-Todos_MaterialModulo2/Sesion1/ejercicio2.c
@@ -5,40 +5,77 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/uio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 
 const int num_bytes = 80;
 
-int main(int argc, char * argv[]){
-	int fdind; // entrada.txt
+int main(int argc, char *argv[])
+{
+	int fdin;  // entrada.txt
 	int fdout; // salida.txt
 	char buffer[num_bytes];
-	int leidos; 
-	int cont = 1;
-	const char * mensaje = "Bloque ";
-	char numero;
-	if (argc == 2){
-		if ((fdin = open(argv[1], 0_RDONLY, S_IRUSR | S_IWUSR)) < 0){
+	int leidos;
+	char cont = '1';
+	struct stat st;
+	int size;
+
+	if (argc == 2)
+	{
+		if ((fdin = open(argv[1], O_RDONLY, S_IRUSR | S_IWUSR)) < 0)
+		{
 			printf("\nError %d en open", errno);
 			perror("\nError en open");
-			exit(EXIT_FAILURE);		
+			exit(-1);
+		} else{
+			stat(argv[1],&st);
+			size = st.st_size;
 		}
 	}
-	else{
+	else
+	{
 		fdin = STDIN_FILENO;
 	}
-	
-	if ((fdout = open("salida.txt", 0_CREAT | O_TRUNC | O_WRONGLY, S_IRUSR | S_IWUSR)) < 0){
+
+	if ((fdout = open("salida.txt", O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR)) < 0)
+	{
 		printf("\nError %d en open", errno);
-			perror("\nError en open");
-			exit(EXIT_FAILURE);
+		perror("\nError en open");
+		exit(-1);
 	}
-	
-	while (leidos = read(fdin,buffer,num_bytes) < 1){ // 0 es EOF y -1 es error
-		
-		write(fdout,buffer, leidos);	
+
+	size = size / num_bytes;
+	printf("%i",size);
+	char tamanio = size + '0';
+	leidos = read(fdin,buffer,num_bytes+1);
+	if (leidos > 0){
+		char total [] = "Numero total de bloques: ";
+		write(fdout, total, sizeof(total)-1);
+		write(fdout,&tamanio,1);
+		write(fdout,"\n",1);
 	}
-}	
+	while (leidos > 0)	{
+		char bloque [] = "Bloque ";
+		write(fdout,bloque,sizeof(bloque)-1);
+		write(fdout,&cont,1);
+		write(fdout,"\n",1);
+
+		int escritos =	write(fdout,buffer, leidos);
+		cont++;
+		leidos = read(fdin,buffer,num_bytes+1);
+	}
+	//ponemos al principio el puntero de escritura
+	cont--;
+	lseek(fdout,-cont*num_bytes,SEEK_CUR);
+	char bloque [] = "El numero de bloque es ";
+	write(fdout,bloque,sizeof(bloque)-1);
+	write(fdout,&cont,1);
+	write(fdout,"\n",1);	
+	close(fdin);
+	close(fdout);
+	return 0;
+}
